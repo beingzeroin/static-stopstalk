486c9076b77a125212a01d749e3d5e981faa75ce
Sandeep Wadhwa <sandeep@beingzero.in>
2020-07-17 00:06:50 +0530

    Adding YARN and EJS

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.set('views', __dirname + '/frontend/views');
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.static('frontend'))
-app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/views/index.html'))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
